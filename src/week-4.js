76e4f9499805357808af250379e6417b8983ffc0
Yugenzariah <KeithJhaeron-Cayatoc@live.nmit.ac.nz>
2022-03-16 14:20:35 +1300

    Jweek-4 JavaScript

--- src/week-4.js
+++ src/week-4.js
@@ -47,3 +47,97 @@ if (number === 10){
     else {
     console.log("error")
 }
+
+// const and let statements
+let num = 3
+console.log(num) 
+
+num = "Azariah"
+console.log(num)
+
+//JavaScript comments
+
+/**
+This is 
+a multiple line comment
+*/
+
+var sdv503 = "JavaScript"
+
+var x = 3 
+
+var fullName = "text"
+
+//foo(x,y)
+
+function foo(p1, p2){
+    return p2 - p1 
+}
+
+foo(5, 10) 
+
+function sum(x1, x2){
+    return x1 + x2
+}
+
+sum(10, 3) 
+
+// (=) vs (===)
+
+//statements vs expressions
+var numbers 
+
+//statement
+// var x 
+
+// if (y >= 0) {
+//     x = y 
+// } else{
+//     x = -y
+// }
+// function statements (x) {
+//     if(y >= 0){
+//     return x = -y
+//     } else{
+//         return x = -y
+//     }
+// }
+
+//expression ES6
+
+// var x = y >= 0 ? y : -y 
+
+var word = "Azariah Keith Jhaeron Maing Ambaked Cayatoc" 
+console.log(word.length)
+
+var obj = {} 
+console.log(obj)
+
+obj.foo = 190
+console.log(obj)
+console.log(word.toUpperCase())
+
+//JavaScript values (primitives vs objects)
+
+var obj1 = {}
+var obj2 = {}
+
+console.log(obj1 === obj2)
+
+var number_one = 123 
+var number_two = 123
+
+console.log(number_one === number_two)
+
+// Boolean: true and false
+// Numbers: 1234, 1432.4
+// Strings: 'letters', "letters"
+// non-values: null and undefined 
+
+var str = "Azariah"
+console.log(str.length = 2)
+console.log(str.length)
+
+var numbers = [1, 2, 3, 4, 5]
+console.log(numbers[0] = 10)
+console.log(numbers)
