7b89eb27d94e4f765b2c750bea5335121ae1e9db
Yugenzariah <KeithJhaeron-Cayatoc@live.nmit.ac.nz>
2022-03-30 10:10:40 +1300

    Code for week-6

---
+++ src/week-6.js
@@ -0,0 +1,8 @@
+<><div>What is your name?
+    <input type="text"></input>
+    <button>Send</button>
+</div><script>
+        document.querySelector("button")addEventListener(click",function(){document.querySelector("div").textContent = "Hello," + document.querySelector("input").value};
+        {"}"})
+
+    </script></>
