19b5f3ed04bd0e570da4ed95d997b10b6619b1a4
Yugenzariah <KeithJhaeron-Cayatoc@live.nmit.ac.nz>
2022-04-06 10:28:12 +1200

    addition

--- src/week-7.js
+++ src/week-7.js
@@ -8,4 +8,16 @@ function getArrElement(arr){
 console.log(getArrElement([1, 2, 3, 4]))
 
 //error function
-const getArrElement = arr => arr[0]
+
+//const getArrElement = arr => arr[0]
+
+//Give two arguement, return an array which contains these two arguments
+// return means you are going to use "function"
+
+function createArray(a, b){
+    var arr = [a, b]
+
+    return arr
+}
+
+console.log(createArray(1, 2)) 
