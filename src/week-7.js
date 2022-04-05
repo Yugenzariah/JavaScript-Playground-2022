d42750e0051fcc8d2fde99b4a58a4e9f07ab7d33
Yugenzariah <KeithJhaeron-Cayatoc@live.nmit.ac.nz>
2022-04-06 11:07:16 +1200

    changes

--- src/week-7.js
+++ src/week-7.js
@@ -21,3 +21,14 @@ function createArray(a, b){
 }
 
 console.log(createArray(1, 2)) 
+
+// Write a function to reverse an array
+// Example: reverseArray([1, 2, 3, 4]) -> [4, 3, 2, 1]
+
+// function reverseArray(arr) {
+//     return arr.reverse()
+// }
+
+const reverseArray = arr => arr.reverse();
+
+console.log(reverseArray([1,2,3,4,5,6,7,8,9]))
