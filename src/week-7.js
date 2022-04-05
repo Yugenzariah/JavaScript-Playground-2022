167a666cabdea59260793337cf8c82aa2cc7baa4
Yugenzariah <KeithJhaeron-Cayatoc@live.nmit.ac.nz>
2022-04-06 09:32:09 +1200

    week-7

---
+++ src/week-7.js
@@ -0,0 +1,11 @@
+//create a function that takes an array containing only numbers and return the first element.
+//Example: getArrElement([1, 2, 3, 4]) -> 1
+
+function getArrElement(arr){
+    return arr[0]
+}
+
+console.log(getArrElement([1, 2, 3, 4]))
+
+//error function
+const getArrElement = arr => arr[0]
