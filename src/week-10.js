ff00c54a3c55272d077184d907eae8dcc9eb9e68
Yugenzariah <KeithJhaeron-Cayatoc@live.nmit.ac.nz>
2022-05-11 10:11:03 +1200

    new notes

--- src/week-10.js
+++ src/week-10.js
@@ -21,3 +21,21 @@ function subarrays(arr,n) {
         return [... new Set(x)]
     }
 } 
+
+/* Create a function that takes two arrays of numbers and return an array of numbers
+which are common to both given arrays */ 
+
+// Returns an array of numbers that are common in the two arrays given 
+function arrCommon(arr, arr2) {
+    finalArr = []
+    // Loops through the first array
+    for (let i = 0; i < arr.length; i++) {
+        // Checks if the item is in the other array
+        if (arr2.includes(arr[i])) {
+            finalArr.push(arr[i])
+        }
+    }
+    return finalArr
+}
+console.log(arrCommon([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], [1, 3, 5, 7, 8, 9, 10]))
+// returns [1, 3, 5, 7, 8, 9, 10] 
